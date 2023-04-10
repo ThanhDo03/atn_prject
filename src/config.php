4d8756748a7620c582700d759415ee9caf00d5b3
ThanhDo <tranthanhdo2407@gmail.com>
2023-04-10 18:39:07 +0700

    l3

--- src/config.php
+++ src/config.php
@@ -3,11 +3,12 @@
 
 $db_username = 'dott';
 $db_password = 'dbPass@123';
-$db_name = 'ToysKid_Project';
+$db_name = 'toyskid_project';
 $db_host = 'db';
 
 $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
 if($mysqli->connect_error){
     die('Error : (' .$mysqli->connect_error .') '.$mysqli->connect_error);
 }
+
 ?>
